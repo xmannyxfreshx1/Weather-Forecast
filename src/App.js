f285975048f5445d63cb7ab93887be622e4b8b48
Manuel Antonio Ramos Gonzalez <xmannyxfreshx1@gmail.com>
2018-07-13 13:43:59 -0400

    fixed https issue

--- src/App.js
+++ src/App.js
@@ -41,7 +41,7 @@ class App extends Component {
         e.preventDefault();
         const city = e.target.elements.city.value;
         const country = e.target.elements.country.value;
-        const api_call= await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city},${country}${API_KEY}`);
+        const api_call= await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city},${country}${API_KEY}`);
         const data = await api_call.json();
 
 
