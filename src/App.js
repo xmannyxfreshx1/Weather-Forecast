30f8802913a7b110b521242cdce54560cd1a837d
Manuel Antonio Ramos Gonzalez <xmannyxfreshx1@gmail.com>
2018-07-13 12:55:51 -0400

    added weather component and appjs

--- src/App.js
+++ src/App.js
@@ -1,21 +1,164 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Titles from './Components/Titles';
+import Form from './Components/Form';
+import Weather from './Components/Weather';
+import  Moment  from 'react-moment';
+import Ionicon from 'react-ionicons';
+
+
+const API_KEY='&APPID=5d0f96e287e14ac1b4f50479acb18f9f';
+
+const iconNames = {
+    Default:'ios-time',
+    Clear: 'ios-sunny' ,
+    Rain: 'ios-rainy' ,
+    Thunderstorm: 'ios-thunderstorm' ,
+    Clouds: 'ios-cloudy' ,
+    Snow: 'ios-snow' ,
+    Drizzle: 'ios-umbrella' ,
+};
+
+
+
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
+
+    state = {
+        temperature:null,
+        weather:'Default',
+        city:null,
+        country:null,
+        humidity:null,
+        description:null,
+        error:null,
+        list: [null],
+        forecastClicked: false,
+        weatherClicked: false,
+    };
+
+    getWeather = async (e) =>{
+        e.preventDefault();
+        const city = e.target.elements.city.value;
+        const country = e.target.elements.country.value;
+        const api_call= await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city},${country}${API_KEY}`);
+        const data = await api_call.json();
+
+
+        if(data.cod === '404') {
+            this.setState({
+                temperature: null,
+                weather:null,
+                city: null,
+                country: null,
+                humidity: null,
+                description: null,
+                error: "ENTER A VALID CITY AND COUNTRY"
+            });
+
+            console.log(data);
+        }else{
+            this.setState({
+                temperature: Math.round((data.main.temp) * (9 / 5) - 459.67),
+                weather:data.weather[0].main,
+                city: data.name,
+                country: data.sys.country,
+                humidity: data.main.humidity,
+                description: data.weather[0].description,
+                error: "",
+                weatherClicked:true
+            });
+        }
+    };
+
+    getForecast = async (e) => {
+        e.preventDefault();
+        const city = this.state.city;
+        const country = this.state.country;
+        const api_call = await fetch (`https://api.openweathermap.org/data/2.5/forecast?q=${city},${country}&appid=3a5fbdb21d0c6479ff9cff57c0e067bc&units=imperial`);
+        const data = await api_call.json();
+        this.setState({
+            list: data.list,
+            forecastClicked: true
+        });
+
+    };
+
+
+
+
+    /*
+
+       constructor(props) {
+           super(props);
+           this.state = {
+               error: null,
+               isLoaded: false,
+               items: []
+
+
+           };
+
+       }
+   */
+
+    render() {
+        let newList = [];
+        {this.state.forecastClicked && this.state.list.map((value, index, array) => {return newList = [array[7], array[15], array[23], array[31], array[39]]} )}
+
+        return (
+            <div>
+
+                <div className="wrapper">
+                    <div className="main">
+                        <div className="container-fluid">
+                            <div className="row">
+                                <div className="col-xs-5 title-container">
+                                    <Titles weatherClicked={this.state.weatherClicked} weatherState={this.state.weather}/>
+                                </div>
+                                <div className="col-xs-7 form-container">
+                                    <Form getWeather={this.getWeather}/>
+                                    <Weather
+                                        temperature={this.state.temperature}
+                                        city={this.state.city}
+                                        country={this.state.country}
+                                        humidity={this.state.humidity}
+                                        description={this.state.description}
+                                        error={this.state.error}
+                                    />
+                                    {this.state.weatherClicked && <button onClick={this.getForecast}>5-Day Forecast</button>}
+                                    <div className="forecast">
+                                        <div className="card">
+
+                                            {this.state.forecastClicked && newList.map((value, index, array) =>
+
+                                                <div key={index}  className="card--content">
+                                                    <div className="card--content--left">
+                                                        <Ionicon color={"white"} fontSize={"60px"} icon={iconNames[value.weather[0].main]}/>
+                                                    </div>
+
+                                                    <div className="card--content--right">
+                                                        <div>Date: <span className="forecast_value"><Moment format="MM/DD/YYYY" unix>{value.dt}</Moment></span></div>
+                                                        <div>Temperature: <span className="forecast_value">{Math.round(value.main.temp)}°</span></div>
+                                                        <div>Humidity: <span className="forecast_value">{value.main.humidity}%</span></div>
+                                                        <div>Description: <span className="forecast_value">{value.weather[0].description}</span></div>
+                                                    </div>
+
+                                                </div>)}
+
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+
+        );
+    }
 }
+
 
 export default App;
---
+++ src/Components/Titles.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import Ionicon from 'react-ionicons';
+
+const iconNames = {
+    Default:'ios-time',
+    Clear: 'ios-sunny' ,
+    Rain: 'ios-rainy' ,
+    Thunderstorm: 'ios-thunderstorm' ,
+    Clouds: 'ios-cloudy' ,
+    Snow: 'ios-snow' ,
+    Drizzle: 'ios-umbrella' ,
+};
+
+
+const Titles = props => (
+    <div>
+        <h1 className="title-container__title">Weather Finder</h1>
+        {props.weatherClicked && <Ionicon color={"white"} fontSize={"300px"} icon={iconNames[props.weatherState]} />}
+        <p className="title-container__subtitle">Find current weather, 5-day forecast, and more</p>
+    </div>
+);
+
+export default Titles;
